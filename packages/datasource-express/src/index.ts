bac60124ef4a7e5fe55ce68b696f0c30eb62eec3
Ciro Scales <ultimatefurreddragon@gmail.com>
2025-07-02 23:58:58 -0600

    fix(datasource-express): provide a `baseURL` instead of an axios instance

--- packages/datasource-express/src/index.ts
+++ packages/datasource-express/src/index.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import type { Datasource } from "@geckobot/datasource";
 import type { AxiosInstance } from "axios";
 
@@ -9,8 +10,8 @@ export default class ExpressDatasource implements Datasource {
   readonly projects: ProjectsService;
   readonly projectTags: ProjectTagsService;
 
-  constructor(axios: AxiosInstance) {
-    this.axios = axios;
+  constructor(baseURL: string) {
+    this.axios = axios.create({ baseURL });
     this.projects = new ProjectsService(this.axios);
     this.projectTags = new ProjectTagsService(this.axios);
   }
